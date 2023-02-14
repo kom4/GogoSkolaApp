90c20c061643546caf73d2a33fdf1159b3a0d3e9
damir koma <damir.komic@gmail.com>
2023-02-14 21:23:25 +0100

    Add project files.

---
+++ GogoSkola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GogoSkola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
